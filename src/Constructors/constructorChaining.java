dfedf23d49d930fddcdf3bc4031cb3999791fade
NehaGoelMittal <64191097+NehaGoelMittal@users.noreply.github.com>
2022-10-07 21:32:05 +0530

    Update constructorChaining.java
    commented System.out.println("add testing");

--- src/Constructors/constructorChaining.java
+++ src/Constructors/constructorChaining.java
@@ -21,7 +21,7 @@ public class constructorChaining {
 	{
 		
 		System.out.println("add");
-		System.out.println("add testing");
+		//System.out.println("add testing");
 		
 	}
 	public constructorChaining(String studentName, int rollNum)
